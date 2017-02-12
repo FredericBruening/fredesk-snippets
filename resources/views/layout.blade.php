15f99a3e91923858927056cd6901d1db07a4e81f
Frederic Brüning <contact@fredesk.com>
2017-02-12 14:47:49 +0100

    v 1.0 --production

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -34,7 +34,7 @@
             </header>
           </div>
         <div class="hero-body">
-            <div class="container has-text-centered">
+            <div class="container has-text-centeredn">
                 <h1 class="title">
                     &lt;!-- Explore Snippets and &lt;/&gt; --&gt;
                 </h1>
